0b3447681baeed444699465ffb06c01c8b05f0a6
dab150 <cironid12@gmail.com>
2017-02-21 22:24:55 -0500

    Initial commit

---
+++ JSONParser/main.cpp
@@ -0,0 +1,21 @@
+/*	
+	JSON Parser
+	Written by Daniel Cironi
+	February 21, 2017
+
+	Description:
+	This program accepts a JSON Document as input
+	and outputs information about the document.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "hello world! \n";
+
+	cin.get(); //just for testing
+	return 0;
+}
